9affca97aabf74022dde2146d7564892a9d55ef5
Eric Wells <ericwells00@gmail.com>
2017-01-11 19:21:37 -0600

    Add files via upload

---
+++ Guesser.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wizard.craft;
+
+/**
+ *
+ * @author Eric
+ */
+public class Guesser {
+    
+    
+    public void Hello() {
+        System.out.println("");
+                System.out.println("");
+
+                        System.out.println("");
+
+                                System.out.println("");
+
+                                        System.out.println("");
+
+                                                System.out.println("");
+
+            System.out.println("===========================================================");
+                 System.out.println("\t\tMy guess is" +"\n\t\t........");
+                            System.out.println("\t\t.......");
+                            System.out.println("\t\t......");
+                            System.out.println("\t\t.....");
+                            System.out.println("\t\t....");
+                            System.out.println("\t\t...");
+                            System.out.println("\t\t..");
+                            System.out.println("\t\t.");
+                System.out.println("===========================================================");
+        System.out.println("");
+        System.out.println("");
+                            
+        
+}
+    
+}
